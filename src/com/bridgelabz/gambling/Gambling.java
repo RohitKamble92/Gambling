2ebd862b1eb3a682dc117b8affdaa87df05e6509
Rohit <rohitkamble10792@gmail.com>
2022-05-28 16:42:36 +0530

    Initialized Variables

--- src/com/bridgelabz/gambling/Gambling.java
+++ src/com/bridgelabz/gambling/Gambling.java
@@ -2,7 +2,9 @@ package com.bridgelabz.gambling;
 
 public class Gambling {
     public static void main(String[] args) {
-        System.out.println("Welcome to gambling Game Program");
+        int DAILY_STAKE =100;
+        int GAME_BET=1;
+
 
     }
 
